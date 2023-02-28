fc32e3f52821d9d0a7e943534bf856d26f2e37d5
Guyutongxue <guyutongxue@163.com>
2023-02-28 14:32:54 +0800

    sync with "starter kit"

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,6 @@
 add_rules("mode.debug", "mode.release")
 
 target("mini_lisp")
-    add_files("src/*.cpp")
-    set_languages("c++20")
+  add_files("src/*.cpp")
+  set_languages("c++20")
+  set_targetdir("bin")
